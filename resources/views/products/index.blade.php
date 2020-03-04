a7925ed5ea4296eef5cb501757434c8437821fd2
RAMIDGE <gael.grb@gmail.com>
2020-03-04 17:23:30 +0100

    add carousel and scripts
    add carousel to product.index
    add bootstrap js  script to master

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -1,6 +1,34 @@
 @extends('layout.master')
 
 @section('content')
+
+<div id="carouselExampleIndicators" class="carousel slide col-md-12 mb-2" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+  </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-100" src="/Images/pcFixe.jpg" alt="First slide" style="height:520px;width:100%">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="/Images/pcPortable.jpg" alt="Second slide" style="height:520px;width:100%">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="/Images/PHOTO-PROMO.jpg" alt="Third slide" style="height:520px;width:100%">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+
   @foreach ($products as $product)
     <div class="col-md-6">
       <div class="row no-gutters border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
